26bf0ccc57de9ced7d76ca293183d460864bb22e
wangchunbo <wangchunbo@oceanum.cn>
2020-06-28 17:29:27 +0800

    cmt

--- build/start.sh
+++ build/start.sh
@@ -4,4 +4,4 @@ echo ${path}
 classpaths="${path}/lib/*"
 cps=`echo $classpaths | sed 's/[ ][ ]*/:/g'`
 echo "${cps}"
-java -javaagent:${path}/lib/sigar-loader-1.6.6-rev002.jar -cp ${cps} com.oceanum.ClusterStarter --base-path="$path" $@
+java -javaagent:${path}/lib/sigar-loader-1.6.6-rev002.jar -cp ${cps}:${path}/conf/logback.xml com.oceanum.ClusterStarter --base-path="$path" $@
