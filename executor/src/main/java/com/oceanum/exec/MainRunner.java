a8cfd7014ce5036a939d77d4d7ea27e4d775703e
wangchunbo <wangchunbo@oceanum.cn>
2020-07-22 17:21:20 +0800

    cmt

---
+++ executor/src/main/java/com/oceanum/exec/MainRunner.java
@@ -0,0 +1,14 @@
+package com.oceanum.exec;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class MainRunner {
+    public static void main(String[] args) throws Throwable {
+        String mainClass = args[0];
+        String[] argz = Arrays.copyOfRange(args, 1, args.length);
+        Class<?> clazz = Class.forName(mainClass);
+        Method method = clazz.getMethod("main", String[].class);
+        method.invoke(null, (Object) argz);
+    }
+}
---
+++ executor/src/main/java/com/oceanum/exec/Test.java
@@ -0,0 +1,9 @@
+package com.oceanum.exec;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Arrays.asList(args));
+    }
+}
