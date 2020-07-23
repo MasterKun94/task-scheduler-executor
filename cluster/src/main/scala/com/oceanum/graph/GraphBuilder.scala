d3510abf357d48ca908a20e7e0b57fa8769d847a
wangchunbo <wangchunbo@oceanum.cn>
2020-07-23 18:02:44 +0800

    cmt

--- cluster/src/main/scala/com/oceanum/graph/GraphBuilder.scala
+++ cluster/src/main/scala/com/oceanum/graph/GraphBuilder.scala
@@ -2,7 +2,7 @@ package com.oceanum.graph
 
 import java.util.concurrent.atomic.AtomicInteger
 
-import com.oceanum.graph.Operators.Operator
+import com.oceanum.graph.Operators.{Operator, Start}
 import com.oceanum.graph.StreamFlows.{EndFlow, StartFlow}
 
 import scala.collection.mutable
@@ -19,15 +19,16 @@ class GraphBuilder(val start: StartFlow,
   private val operators: mutable.Map[Int, Operator[_]] = mutable.Map()
   private val edges: mutable.Map[Int, Array[Int]] = mutable.Map()
 
-  private def getIdAndSet(operator: Operator[_]): Int = operators.find(_._2 == operator) match {
-    case Some((i, _)) => i
-    case None =>
-      val id = idValue
-      operators += (id -> operator)
-      id
-  }
 
   def add(from: Operator[_], to: Operator[_]*): GraphBuilder = {
+    def getIdAndSet(operator: Operator[_]): Int = operators.find(_._2 == operator) match {
+      case Some((i, _)) => i
+      case None =>
+        val id = idValue
+        operators += (id -> operator)
+        id
+    }
+
     val fromId = getIdAndSet(from)
     val toIds = to.map(getIdAndSet).toArray
     edges += (fromId -> toIds)
@@ -35,7 +36,22 @@ class GraphBuilder(val start: StartFlow,
   }
 
   def build(): Unit = {
-    ???
+    def build(id: Int, operator: Operator[_]): Unit = {
+      edges.get(id).foreach { arr =>
+        arr.map(i => (i, operators(i)))
+          .foreach(kv => {
+            val (i, sub) = kv
+            val subOperator = operator.connect(sub)
+            operators += (i -> subOperator)
+            build(i, subOperator)
+          })
+        edges - id
+      }
+    }
+    operators.find(_._2.isInstanceOf[Start]) match {
+      case Some((id, start)) => build(id, start)
+      case _ => throw new IllegalArgumentException("需要start算子")
+    }
   }
 }
 
