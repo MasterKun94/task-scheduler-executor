ccbe630a248e244654866c78375a99d4d488019e
wangchunbo <wangchunbo@oceanum.cn>
2020-07-27 17:49:48 +0800

    cmt

--- cluster/src/main/scala/com/oceanum/trigger/HDFSEventListener.scala
+++ cluster/src/main/scala/com/oceanum/trigger/HDFSEventListener.scala
@@ -5,9 +5,12 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
 
 import com.oceanum.common.Environment
+import com.oceanum.file.HDFSFileClient
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hdfs.client.HdfsAdmin
 import org.apache.hadoop.hdfs.inotify.Event.{AppendEvent, CloseEvent, CreateEvent, MetadataUpdateEvent, RenameEvent, UnlinkEvent}
+
+import scala.concurrent.ExecutionContext
 
 /**
  * @author chenmingkun
@@ -19,22 +22,23 @@ class HDFSEventListener {
 
   private val admin: HdfsAdmin = {
     println(Environment.HADOOP_HOME)
+    println(Environment.HADOOP_FS_URL)
     System.setProperty("hadoop.home.dir", Environment.HADOOP_HOME)
     System.setProperty("HADOOP_USER_NAME", "hdfs")
     val configuration = new Configuration()
-    new HdfsAdmin(URI.create("hdfs://192.168.10.136:8022"), configuration)
+    new HdfsAdmin(URI.create(Environment.HADOOP_FS_URL), configuration)
   }
 
   def create(): Unit = {
     val eventStream = admin.getInotifyEventStream()
     while (running.get()) {
       eventStream.take() match {
-//        case event: CreateEvent =>
-//        case event: CloseEvent =>
-//        case event: AppendEvent =>
-//        case event: RenameEvent =>
-//        case event: MetadataUpdateEvent =>
-//        case event: UnlinkEvent =>
+        case event: CreateEvent =>
+        case event: CloseEvent =>
+        case event: AppendEvent =>
+        case event: RenameEvent =>
+        case event: MetadataUpdateEvent =>
+        case event: UnlinkEvent =>
         case event => println(event)
       }
     }
@@ -45,7 +49,18 @@ class HDFSEventListener {
 
 object HDFSEventListener {
   def main(args: Array[String]): Unit = {
-    Environment.loadArgs(args :+ s"--conf=src/main/resources/application.properties")
+    Environment.loadArgs(args :+ s"--conf=cluster/src/main/resources/application.properties")
     new HDFSEventListener().create()
+  }
+}
+
+object HDFSTest {
+  def main(args: Array[String]): Unit = {
+    Environment.loadArgs(args :+ s"--conf=cluster/src/main/resources/application.properties")
+    println(Environment.HADOOP_HOME)
+    println(Environment.HADOOP_FS_URL)
+    val client = new HDFSFileClient()
+    client.upload("E:\\chenmingkun\\task-scheduler-executor\\cluster\\src\\main\\resources\\python.py", "/tmp/")(ExecutionContext.global)
+    Thread.sleep(10000)
   }
 }
